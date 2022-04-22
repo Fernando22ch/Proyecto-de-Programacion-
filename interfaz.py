f94268d0c2f440acbc32fa39d44e64b5760ddf01
JeisonB1 <jeiblanco@estudiantec.cr>
2022-04-22 16:54:15 -0600

    Faltante: registro de actividades y modificar los cursos y asctividades para que se guarden con el formato de date.time y asi usar las horas y poder comparar

--- interfaz.py
+++ interfaz.py
@@ -3,13 +3,14 @@ import logicaproyecto as logica
 
 
 
+
 '''                             MENU DEL PROYECTO                                                                              '''
 #                               INTERFAZ GRAFICA
 
 
 
 def menu_interfaz(): 
-    logica.limpiar_terminal
+    logica.limpiar_terminal()
     while True:
         logica.limpiar_terminal()
         print ("Bienvenido a tu calendario")
@@ -31,6 +32,7 @@ def menu_interfaz():
                     print("Usuario Estudiante")
                     print("1) Iniciar sesion")
                     print("2) Registrarse")
+
                     print("3) Salir")
                     opt= int(input(" Su opcion es: "))
                     match opt:
@@ -38,8 +40,8 @@ def menu_interfaz():
                         
                             #iniciar sesion estudiante*****
                             usuario= input("Usuario: ")
-                            contra= input("Contraseña: ")
-                            logica.us_momentaneo(usuario)
+                            contra= logica.obtener_clave("Contraseña: ")
+                            
                             if logica.validar_estudiante(usuario,contra):
                                 logica.limpiar_terminal()
                                 menu_prin_estudiante() 
@@ -64,7 +66,7 @@ def menu_interfaz():
                 match optt:
                     case 1: #iniciar sesion admin*****
                         usuario= input("Usuario: ")
-                        contraseña= input("Contraseña: ")
+                        contraseña = logica.obtener_clave("Contraseña: ")
                         if logica.validar_admin(usuario,contraseña):
                             
                             menu_prin_administrativo() 
@@ -90,31 +92,52 @@ def menu_interfaz():
 
 def menu_prin_estudiante():
     while True: 
-    
+        logica.limpiar_terminal()
         print("1) Cambio de carrera")
         print("2) Matricular cursos")
         print("3) Agregar actividades")
-        print("4) Salir")
+        print("4) Mostar calendario")
+        print("5) Salir")
         opt= int(input(" Su opcion es: "))
+
         match opt: 
             case 1:
                 # Cambio de Carrera:
                 logica.cambiar_carrera()
 
             case 2:
+                while True:
+                    logica.limpiar_terminal()
                 # Matricular cursos:
-                logica.matricular_cursos()
-
-
+                    print("1) Matricular cursos")
+                    print("2) Modificar estado de cursos")
+                    print("3) Salir")
+                    opttt= int(input(" Su opcion es: "))
+                    match opttt:
+                        case 1:
+                            logica.matricular_cursos()
+                        case 2: 
+                            logica.aprobar_reprobar_cursos()
+                        case 3:
+                            break
+                        case _:
+                            print("Opcion invalida")
 
             case 3:
                 pass  
-                # Agregar actividades 
-
+                # Agregar actividades
+            case 4:
+                logica.calendario()
+                sleep(5)
+            case 5:
+                break
+            case _:
+                print("Opcion invalida")
 
 def menu_prin_administrativo():
     while True: 
-        print("Nombre: Fernando Chaves Calvo, telefono:83220200")
+        logica.limpiar_terminal()
+        print("Bienvenido administrador")
         print("1) Agregar cursos")
         print("2) Agregar carreras")
         print("3) Salir")
--- logicaproyecto.py
+++ logicaproyecto.py
@@ -1,12 +1,9 @@
 #snakecase
-usuario_momentaneo=""
 from time import sleep
-
 from hashlib import md5
 from getpass import getpass
-from typing import cast
-def us_momentaneo(nombre):
-    usuario_momentaneo=nombre
+usuario_momentaneo=''
+
 
 
 
@@ -22,10 +19,10 @@ def obtener_clave(mensaje):
     pswd = getpass(mensaje+": ")
     return (pswd)
 
-usuarios_estudiante=[{'id':1 ,'Nombre':'Jeison Blanco Rojas', 'Carrera':'computacion', 'autenticacion':{'usuario':'jeisonb1','contraseña':'jei123'},
-'cursos_matriculados': {'taller de programacion': '','introduccion a la programacion':'reprobado'}, 'actividades':{'futbol':{'dia':'lunes','horas':'4'}}}]
+usuarios_estudiante=[{'Nombre':'Jeison Blanco Rojas', 'Carrera':'computacion', 'autenticacion':{'usuario':'jeisonb1','contraseña':'jei123'},
+'cursos_matriculados':[{'nombre':'taller de programacion','estado': ''}, {'nombre':'introduccion a la programacion','estado': ''}], 'actividades':{'futbol':{'dia':'lunes','horas':'4'}}}]
 
-usuarios_admin=[{'id':1,'Nombre':'Fernando Chaves Calvo', 'telefono':'83220200', 'autenticacion':{'usuario':'admin','contraseña':'admin'}}]
+usuarios_admin=[{'Nombre':'Fernando Chaves Calvo', 'telefono':'83220200', 'autenticacion':{'usuario':'admin','contraseña':'admin'}}]
 def registrar_estudiante():
     limpiar_terminal()
     nuevo_estudiante = dict()
@@ -42,14 +39,14 @@ def registrar_estudiante():
 
 def registrar_admin():
 
-    limpiar_terminal
+    limpiar_terminal()
     nuevo_admin=dict()
     nuevo_admin['Nombre']=input("Nombre: ")
     nuevo_admin['Telefono']=input("telefono: ")
 
     print("Autenticacion")
-    usuario=input ("\tNombre de usuario: ")
-    contraseña=cifrar(obtener_clave("Digite su contraseña: "))
+    usuario=input ("\t Nombre de usuario: ")
+    contraseña=cifrar(obtener_clave("\t Digite su contraseña: "))
 
 
     nuevo_admin['autenticacion']={'usuario':usuario, 'contraseña': contraseña}
@@ -57,17 +54,22 @@ def registrar_admin():
 
 
 def validar_estudiante(usuario,contraseña):
-    
+    global usuario_momentaneo
     resp=False
+    contraseña=cifrar(contraseña)
     for value in usuarios_estudiante:
+        
         if value['autenticacion']['usuario']==usuario and value['autenticacion']['contraseña']==contraseña:
             resp=True
-            us_momentaneo(value['Nombre'])
+            usuario_momentaneo=(value['Nombre'])
             break
     return(resp)
+
 def validar_admin(usuario,contraseña):
     resp=False
+    contraseña=cifrar(contraseña)
     for value in usuarios_admin:
+        
         if value['autenticacion']['usuario']==usuario and value['autenticacion']['contraseña']==contraseña:
             resp=True
             break
@@ -76,7 +78,8 @@ def validar_admin(usuario,contraseña):
 
     
 
-cursos=[{'nombre': 'taller de programacion','creditos':'2','horas lectivas':'3','inicia':'07-02-22','finaliza':'23-06-22','horario de clases':'Jueves y viernes a las 5pm','carreras':'computacion'}]
+cursos=[{'nombre': 'taller de programacion','creditos':'2','horas lectivas':'3','inicia':'07-02-22','finaliza':'23-06-22','dia de clases':'viernes','hora de clases':'5pm','carreras':'computacion'}]
+
 def registros_de_cursos():
     curso_nuevo=dict()
     
@@ -86,24 +89,27 @@ def registros_de_cursos():
     curso_nuevo['horas lectivas']=int(input("horas lectivas: "))
     curso_nuevo['inicia']=input ("fecha de inicio del curso: ")
     curso_nuevo['finaliza']=input ("fecha de finalizacion del curso: ")
-    curso_nuevo['horario de clases']=input("horarios del curso: ")
+    curso_nuevo['dia de clases']=input("dia del curso: ")
+    curso_nuevo['hora de clases']=input("hora del curso: ")
     curso_nuevo['carreras']=carre=input("carreras que pertenece el curso: ")
     cursos.append(curso_nuevo)
-    for value in carerras_cursos:
-        if value['nombre']==carre:
-            carerras_cursos['cursos_esp']+=c
+    # for value in carerras_cursos:
+    #     if value['nombre']==carre:
+    #         carerras_cursos['cursos_esp']+=c
    
-carerras_cursos=[{'nombre':'computacion','cursos_esp':['taller de programacion','introduccion a la programacion']}]
+carerras_cursos={'nombre':'computacion','cursos_esp':['taller de programacion','introduccion a la programacion']}
 carreras={'computacion',"agronomia"}
 def registros_de_carreras():
+    limpiar_terminal()
     print("Carreras existentes:")   
     print(carreras)
     carre_nueva=(input("Nombre de la carrera: "))
     carreras.add(carre_nueva)
-    carerras_cursos.append(carre_nueva)
+    # carerras_cursos.append(carre_nueva)
 
 def cambiar_carrera():
-    
+    global usuario_momentaneo
+    limpiar_terminal()
     carreranueva= input("nueva carrera:" )
 
     for x in usuarios_estudiante:
@@ -115,13 +121,94 @@ def cambiar_carrera():
     
 
 def matricular_cursos():
+    limpiar_terminal()
     global usuario_momentaneo
     a_matricular = input("Cual curso desea matricular:")
     for x in usuarios_estudiante:
+        
         if x['Nombre'] == usuario_momentaneo:
+            
             for curso in cursos:
-                if curso['carreras'] == x['Carrera']:
-                    x['cursos_matriculados'][a_matricular] = ""
+                if curso['carreras'] == x['Carrera'] and x['cursos_matriculados']!=a_matricular:
+                    x['cursos_matriculados'][a_matricular]['estado']=''
+                else:
+                    print("Curso Incorrecto o ya matriculado")
+
+def aprobar_reprobar_cursos():
+    limpiar_terminal()
+    global usuario_momentaneo
+    for x in usuarios_estudiante:
+        
+        if x['Nombre'] == usuario_momentaneo:
+            curso_modificar = input("Cual curso desea modificar el estado:")
+
+            for curso in cursos:
+                if curso['carreras'] == x['Carrera'] and x['cursos_matriculados'] == curso_modificar:
+                    x['cursos_matriculados'][curso_modificar]['estado']= estado = input("Curso aprobado o reprobado:")
+                else:
+                    print("Curso Incorrecto o no matriculado")
+
+lunes=[]
+martes=[]
+miercoles=[]
+jueves=[]
+viernes=[{'taller de programacion':'5pm'}]
+sabado=[]
+domingo=[]
+
+
+#errores
+def registro_de_asistencia_lectiva():
+    global usuario_momentaneo
+   
+    for x in usuarios_estudiante:
+        if x['Nombre'] == usuario_momentaneo:
+            curso_pendiente=x['cursos_matriculados']
+            for y in curso_pendiente:
+                if y['estado']=='':
+                    for curso in cursos:
+                        if curso['nombre']==y['nombre']:
+                            dia=curso['dia de clases']
+                            if dia==lunes:
+                                lunes.append(curso['nombre'] and curso['hora de clases'])
+                            elif dia==martes:
+                                martes.append(curso['nombre'] and curso['hora de clases'])
+                            elif dia==miercoles:
+                                miercoles.append(curso['nombre'] and curso['hora de clases'])
+                            elif dia==jueves:
+                                jueves.append(curso['nombre'] and curso['hora de clases'])
+                            elif dia==viernes:
+                                viernes.append(curso['nombre'] and curso['hora de clases'])
+                            elif dia==sabado:
+                                sabado.append(curso['nombre'] and curso['hora de clases'])
+                            elif dia==domingo:
+                                domingo.append(curso['nombre'] and curso['hora de clases'])
+
+def calendario():
+    limpiar_terminal()
+    registro_de_asistencia_lectiva()
+    #aqui va registrar actividades que modifica las listas ya creadas que tambien tienen cursos
+    print("Lunes:")
+    print(lunes)
+
+    print("Martes:")
+    print(martes)
+    print("Miercoles:")
+    print(miercoles)
+    print("Jueves:")
+    print(jueves)
+    print("Viernes:")
+    print(viernes)
+    print("Sabado:")
+    print(sabado)
+    print("Domingo:")
+    print(domingo)
+                    
+                        
+                       
+
+
+
 
 
 
@@ -131,5 +218,5 @@ def matricular_cursos():
 
 
 
-'''             prueba de funciones 
-'''
+# '''             prueba de funciones 
+# '''
